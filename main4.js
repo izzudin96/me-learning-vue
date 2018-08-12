108811bea84fc84dc44c5b87d02cd4e4b9586a84
Izzudin Anuar <izzudinanuar96@gmail.com>
2018-08-12 15:48:37 +1200

    tasks?

--- main4.js
+++ main4.js
@@ -1,3 +1,19 @@
+Vue.component('task-list', {
+    template: '<div><task v-for="task in tasks">{{ task.description }}</task></div>',
+
+    data() {
+        return {
+            tasks: [
+                { description: 'Go to the store', completed: true },
+                { description: 'Go to the mall', completed: false },
+                { description: 'Go to the dryer', completed: false },
+                { description: 'Go to the office', completed: true },
+            ]
+        };
+    }
+});
+
+
 Vue.component('task', {
     template: '<li><slot></slot></li>'
 });
